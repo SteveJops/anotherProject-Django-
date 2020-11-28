ad7166dd7a2540243635235821e7a438dbe94104
Steve Jops <steven07kolesiko@gmail.com>
2020-11-28 13:42:34 +0200

    added reversed words count

--- anotherProject/view.py
+++ anotherProject/view.py
@@ -8,7 +8,22 @@ class MyView(View):
 
 
 class ReverseCommand(View):
+
     def get(self, request):
         user_text = request.GET['usertext']
+        number_of_words = self.numberofwords(user_text)
         reverse = user_text[::-1]
-        return render(request, "reverse.html", {'usertext': user_text, 'reversed': reverse})
+        return render(request, "reverse.html",
+                      {'usertext': user_text, 'reversed': reverse, 'numberofwords': number_of_words})
+
+    def numberofwords(self, user_text):
+        count = 0
+        flag = 0
+        for i in range(len(user_text)):
+            if user_text[i] != ' ' and flag == 0:
+                count += 1
+                flag = 1
+            else:
+                if user_text[i] == ' ':
+                    flag = 0
+        return count
